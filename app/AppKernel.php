99dfdee1163212f8986ce1c605ace11fa468301e
jordigracia <Tete@MacBook-Pro-de-Jordi.local>
2017-11-19 21:04:09 +0100

    Guzzle Buyndle Config, Test first api meetup call

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            /* Third Party Bundles */
+            new EightPoints\Bundle\GuzzleBundle\EightPointsGuzzleBundle(),
+            /* Own Bundles */
             new AppBundle\AppBundle(),
         ];
 
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,6 +14,14 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
+
+        /* Testing api connection */
+        $client   = $this->get('eight_points_guzzle.client.api_meetup');
+        //$response = $client->get('/2/cities');
+        //$response = $client->get('/find/locations');
+        $response = $client->get('/find/locations?photo-host=public&sig_id=241706216&sig=c769c936d88d4e9b75dddfc4c92f1c9a578b1e13');
+        echo $response->getBody();
+
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
